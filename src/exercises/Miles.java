30f413a2e98ac2d78212bfeea06d38eb5278a8ef
Angela Polly <agpolly@gmail.com>
2022-01-26 13:45:13 -0500

    add part 3

--- src/exercises/Miles.java
+++ src/exercises/Miles.java
@@ -3,4 +3,20 @@ package exercises;
 import java.util.Scanner;
 
 public class Miles {
+    public static void main (String[] args) {
+        double milesDriven;
+        double gasUsed;
+        double milesPerGallon;
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.println("Enter the number of miles driven: ");
+        milesDriven = input.nextDouble();
+        System.out.println("Enter the number of gallons of gas used: ");
+        gasUsed = input.nextDouble();
+        input.close();
+
+        milesPerGallon = (milesDriven / gasUsed);
+        System.out.println("The miles per gallon is: " + milesPerGallon);
+    }
 }
