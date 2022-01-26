0774b4879decc3220a9e6a3a8c97de6e5a3d9a61
Angela Polly <agpolly@gmail.com>
2022-01-26 13:34:14 -0500

    exercises 1 and 2

---
+++ src/exercises/Alice.java
@@ -0,0 +1,12 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Alice {
+    public static void main (String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
---
+++ src/exercises/Area.java
@@ -0,0 +1,22 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main (String[] args) {
+        double length;
+        double width;
+        double area;
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.println("Enter the length of the rectangle: ");
+        length = input.nextDouble();
+        System.out.println("Enter the width of the rectangle: ");
+        width = input.nextDouble();
+        input.close();
+
+        area = (length * width);
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
